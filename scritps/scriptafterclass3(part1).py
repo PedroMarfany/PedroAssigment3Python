9e2739a79a1a81b5bb4c89748130d490a72e3c64
PedroMarfany <156207142+PedroMarfany@users.noreply.github.com>
2024-01-14 15:58:22 +0100

    Estructura_Principal(click)

---
+++ scritps/scriptafterclass3(part1).py
@@ -0,0 +1,18 @@
+"""
+Script to filter info of the dataset
+"""
+
+import pandas as pd 
+import click 
+
+@click.command()
+@click.option('--title', prompt='Enter film title', help='The title of the film')
+@click.option('--year', prompt='Enter release year', type=int, help='The release year of the film')
+@click.option('--tickets_sold', prompt='Enter tickets sold', type=int, help='Number of tickets sold for the film')
+@click.option('--ticket_price', prompt='Enter ticket price', type=float, help='Price of each movie ticket')
+
+def main(title, year, tickets_sold, ticket_price)
+
+
+if _name_ == '_main_':               
+    main()
